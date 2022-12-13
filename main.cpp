c2cfbc8cd29fa49de9ea672ef52708b4c1ca8edb
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 11:42:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 #include "lib.h"
 using namespace std;
 
+
 int main(){
     char arraylista[10][20];
     char arraynome[20];
-  
+
     for (int i=0; i<10; i++){
         cin >> arraylista[i];
     }
     cin >> arraynome;
 
-    int riga = controllo_nome(arraylista, arraynome);
-    if (riga==11){
+    if (controllo(arraylista, arraynome)==100){
         cout << "non presente";
     }
-    else cout << riga;
+    else cout << controllo(arraylista, arraynome);
     return 0;
 }
