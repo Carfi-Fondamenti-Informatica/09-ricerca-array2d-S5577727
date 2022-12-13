4f02643923af18b450d0141f3653c5001473dbcb
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 22:45:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,19 @@
 #include "lib.h"
 using namespace std;
 
-
 int main(){
-    char elenco[10][20];
-    for(int i=0; i<10; i++) {
-        cin >> elenco[i];
-    }
-
-    char nome[20];
-    cin >> nome;
-
-    int posizione = cercaNome(elenco, nome);
-    if(posizione == -1){
-        cout << nome << "non presente";
-    }
-    else{
-        cout << posizione;
-    }
-    return 0;
+	char elenco[10][20];
+	for(int i=0; i<10; i++)
+		cin >> elenco[i];
+	
+	char nome[20];
+	cin >> nome;
+	
+	int posizione = cercaNome(elenco, nome);
+	if(posizione == -1)
+		cout << "non presente";
+	else
+		cout << posizione;
+	
+	return 0;
 }
