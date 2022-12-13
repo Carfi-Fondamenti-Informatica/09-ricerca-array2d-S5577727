0a8646725f84d829120a1565af15300bed1a6c2b
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 22:36:28 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef ESERCITAZIONE9_LIB_H
 #define ESERCITAZIONE9_LIB_H
-int controllo(char lista[][20], char nome[20]);
+int cercaNome(char elenco[10][20], char nome[20]);
 #endif //ESERCITAZIONE9_LIB_H
