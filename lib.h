bcbeeac7317002746f70c89b602e8cdb5dbe38ab
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 11:42:54 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef UNTITLED3_LIB_H
 #define UNTITLED3_LIB_H
-int controllo_nome(char arraylista, char arraynome);
+int controllo( char arraylista[10][20], char arraynome[20]);
 #endif //UNTITLED3_LIB_H
