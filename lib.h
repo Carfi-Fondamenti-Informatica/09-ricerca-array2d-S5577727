092da34c5f8fa761129fd26389ef6f8c6f36c3e4
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 22:11:57 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef ESERCITAZIONE9_LIB_H
 #define ESERCITAZIONE9_LIB_H
-int controllo(char lista[10][20], const char nome[20]);
+int controllo(char lista[10][20], char nome[20]);
 #endif //ESERCITAZIONE9_LIB_H
