b07de9b14629467fa7a2299d1017003199b69372
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 10:04:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+int controllo_nome(char arraylista[], char arraynome[]){
+    int riga;
+    for (riga=0; riga<10; riga++){
+        if (arraylista[riga] == arraynome[riga]){
+            return riga;
+        }
+        else riga=11;
+    }
+    return riga;
+}
