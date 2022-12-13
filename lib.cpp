3ea3ac9a08782c43defaeff783d7c28dcaf44337
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 22:05:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,31 +1,10 @@
-//
-// Created by nvand on 10/12/2022.
-//
-
 #include "lib.h"
 
-int controllo(char arraylista[10][20],char arraynome[]){
-    int riga;
-    bool controllo;
-
-    for (riga=0; riga<10; riga++){
-
-        for (int colonna=0; colonna<20;colonna++){
-
-            if (arraylista[riga][colonna] == arraynome[colonna]){
-                controllo=true;
-            }
-            else {
-                controllo=false;
-                colonna=20;
-            }
-        }
-        if (controllo){
-            return riga;
+int controllo(char lista[][20], const char nome[20]){
+    for (int i=0; i<10; i++){
+        if (lista[i] == nome){
+            return i;
         }
     }
-    if (!controllo){
-        riga=100;
-        return riga;
-    }
+    return -1;
 }
