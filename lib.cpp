e7ef4a4fd8dec497c87508e3fdb4c8fe54c04804
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-13 22:42:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,16 @@
 #include "lib.h"
 
-int controllo(char lista[][20], const char nome[20]){
-    for (int i=0; i<10; i++){
-        if (lista[i] == nome){
-            return i;
+int cercaNome(char elenco[10][20], char nome[20]){
+    for(int i=0; i<10; i++){
+     for(int j=0; j<20; j++){
+        while(elenco[i][j] != '\0' && nome[j] != '\0'){
+            if(elenco[i][j] != nome[j])
+                break;
+            j++;
         }
+        if(nome[j] == '\0')	// Fine stringa raggiunta
+            return i;
+     }
     }
     return -1;
 }
